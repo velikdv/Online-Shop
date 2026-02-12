da0a8da44139682a50989224c51f7b9722253be1
Denis Velikotskii <d.velikotskii@topdesk.com>
2026-02-12 09:22:45 +0100

    Finish the base functionality

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,8 +1,53 @@
+const Product = require('./product.model');
+
 class Cart {
   constructor(items = [], totalQuantity = 0, totalPrice = 0) {
     this.items = items;
     this.totalQuantity = totalQuantity;
     this.totalPrice = totalPrice;
+  }
+
+  async updatePrices() {
+    const productIds = this.items.map(function (item) {
+      return item.product.id;
+    });
+
+    const products = await Product.findMultiple(productIds);
+
+    const deletableCartItemProductIds = [];
+
+    for (const cartItem of this.items) {
+      const product = products.find(function (prod) {
+        return prod.id === cartItem.product.id;
+      });
+
+      if (!product) {
+        // product was deleted!
+        // "schedule" for removal from cart
+        deletableCartItemProductIds.push(cartItem.product.id);
+        continue;
+      }
+
+      // product was not deleted
+      // set product data and total price to latest price from database
+      cartItem.product = product;
+      cartItem.totalPrice = cartItem.quantity * cartItem.product.price;
+    }
+
+    if (deletableCartItemProductIds.length > 0) {
+      this.items = this.items.filter(function (item) {
+        return deletableCartItemProductIds.indexOf(item.product.id) < 0;
+      });
+    }
+
+    // re-calculate cart totals
+    this.totalQuantity = 0;
+    this.totalPrice = 0;
+
+    for (const item of this.items) {
+      this.totalQuantity = this.totalQuantity + item.quantity;
+      this.totalPrice = this.totalPrice + item.totalPrice;
+    }
   }
 
   addItem(product) {
@@ -34,7 +79,7 @@ class Cart {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
       if (item.product.id === productId && newQuantity > 0) {
-        const cartItem = {...item};
+        const cartItem = { ...item };
         const quantityChange = newQuantity - item.quantity;
         cartItem.quantity = newQuantity;
         cartItem.totalPrice = newQuantity * item.product.price;
@@ -42,16 +87,14 @@ class Cart {
 
         this.totalQuantity = this.totalQuantity + quantityChange;
         this.totalPrice += quantityChange * item.product.price;
-        return {updatedItemPrice: cartItem.totalPrice};
+        return { updatedItemPrice: cartItem.totalPrice };
       } else if (item.product.id === productId && newQuantity <= 0) {
-        this.items.splice(i,1);
+        this.items.splice(i, 1);
         this.totalQuantity = this.totalQuantity - item.quantity;
         this.totalPrice -= item.totalPrice;
-        return {updatedItemPrice: 0};
+        return { updatedItemPrice: 0 };
       }
     }
-
-
   }
 }
 
--- models/order.model.js
+++ models/order.model.js
@@ -1,11 +1,14 @@
+const mongodb = require('mongodb');
+
 const db = require('../data/database');
 
 class Order {
+  // Status => pending, fulfilled, cancelled
   constructor(cart, userData, status = 'pending', date, orderId) {
     this.productData = cart;
     this.userData = userData;
     this.status = status;
-    date = new Date(date);
+    this.date = new Date(date);
     if (this.date) {
       this.formattedDate = this.date.toLocaleDateString('en-US', {
         weekday: 'short',
@@ -14,12 +17,63 @@ class Order {
         year: 'numeric',
       });
     }
-
     this.id = orderId;
+  }
+
+  static transformOrderDocument(orderDoc) {
+    return new Order(
+      orderDoc.productData,
+      orderDoc.userData,
+      orderDoc.status,
+      orderDoc.date,
+      orderDoc._id
+    );
+  }
+
+  static transformOrderDocuments(orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
+  }
+
+  static async findAll() {
+    const orders = await db
+      .getDb()
+      .collection('orders')
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    const orders = await db
+      .getDb()
+      .collection('orders')
+      .find({ 'userData._id': uid })
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findById(orderId) {
+    const order = await db
+      .getDb()
+      .collection('orders')
+      .findOne({ _id: new mongodb.ObjectId(orderId) });
+
+    return this.transformOrderDocument(order);
   }
 
   save() {
     if (this.id) {
+      const orderId = new mongodb.ObjectId(this.id);
+      return db
+        .getDb()
+        .collection('orders')
+        .updateOne({ _id: orderId }, { $set: { status: this.status } });
     } else {
       const orderDocument = {
         userData: this.userData,
@@ -28,7 +82,7 @@ class Order {
         status: this.status,
       };
 
-      db.getDb().collection('orders').insertOne(orderDocument);
+      return db.getDb().collection('orders').insertOne(orderDocument);
     }
   }
 }
